85ecd3265a49f886f3cf60b65d753b4a839a7ca2
Jaime Pinzon <jaime@evilmeteor.com>
2024-10-02 11:46:13 -0500

    Implement binary search, including visualization

--- binary_search.go
+++ binary_search.go
@@ -1,10 +1,84 @@
-package binary_search
+package main
 
 import (
-	"github.com/gofiber/fiber/v3"
+	"errors"
+	"fmt"
 )
 
-func binary_search() {
-	app := fiber.New()
+func main() {
+	list := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	item := 5
 
+	index, err := binary_search(list[:], item)
+
+	if err != nil {
+		fmt.Printf("Error: Item %d not found.\n", item)
+	} else {
+		fmt.Printf("Item %d found at index %d.\n", item, index)
+	}
+}
+
+func binary_search(list []int, item int) (int, error) {
+	low := 0
+	high := len(list) - 1
+	count := 0
+
+	for low <= high {
+		count += 1
+		mid := (low + high) / 2
+		guess := list[mid]
+
+		visualize(list[:], low, high, mid, count)
+
+		if guess == item {
+			return mid, nil
+		}
+
+		if guess < item {
+			low = mid + 1
+		}
+
+		if guess > item {
+			high = mid - 1
+		}
+
+	}
+
+	return -1, errors.New("Not Found")
+}
+
+func visualize(list []int, low int, high int, mid int, count int) {
+	fmt.Printf("%d: ", count)
+
+	for i := 0; i < len(list); i++ {
+		switch {
+		case i < low:
+			fmt.Print("   ")
+
+		case i == low:
+			fmt.Print("{ ")
+			switch {
+			case low == high:
+				fmt.Printf("[%d] }", list[i])
+			case i == mid:
+				fmt.Printf("[%d]", list[i])
+			default:
+				fmt.Printf(" %d ", list[i])
+			}
+
+		case i == mid:
+			fmt.Printf("[%d]", list[i])
+
+		case i == high:
+			fmt.Printf(" %d  }", list[i])
+
+		case i > high:
+			fmt.Print("\n")
+			return
+
+		default:
+			fmt.Printf(" %d ", list[i])
+		}
+	}
+	fmt.Print("\n")
 }
