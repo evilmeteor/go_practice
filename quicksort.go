0f6ad638d7166a419e9f06c08198c3e6eaeab654
Jaime Pinzon <jaime@evilmeteor.com>
2024-10-09 02:34:03 -0500

    Add quicksort implementation

--- quicksort.go
+++ quicksort.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	arr := [10]int{5, 10, 8, 6, 7, 2, 9, 4, 1, 3}
+	//arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	res := quicksort(arr[:])
 	fmt.Println(res)
 }
@@ -15,12 +16,12 @@ func quicksort(arr []int) []int {
 		return arr
 	}
 
-	pivot := arr[0]
+	pivot := arr[(len(arr) / 2)]
 	left := []int{}
 	middle := []int{}
 	right := []int{}
 
-	for i := 1; i < len(arr); i++ {
+	for i := 0; i < len(arr); i++ {
 		if arr[i] < pivot {
 			left = append(left, arr[i])
 		} else if arr[i] > pivot {
@@ -29,9 +30,6 @@ func quicksort(arr []int) []int {
 			middle = append(middle, arr[i])
 		}
 	}
-	middle = append(middle, pivot)
-
-	fmt.Println(pivot, left, middle, right)
 
 	return append(append(quicksort(left), middle...), quicksort(right)...)
 
